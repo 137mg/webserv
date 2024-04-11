707740516eefbc3513aee0c9582400f77862bc6c
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2024-04-11 16:42:17 +0200

    concise class functions

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/09 13:19:11 by juvan-to      #+#    #+#                 */
-/*   Updated: 2024/04/09 14:29:34 by juvan-to      ########   odam.nl         */
+/*   Updated: 2024/04/11 16:39:49 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,10 @@ class Server
 		void	run(void);
 		void	config(void);
 		void	handleRequest(std::string buffer);
+		void	createSocket(void);
+		void	bindSocket(void);
+		void	acceptConnection(void);
+		void	processConnection(int client_fd);
 		
 };
 
--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/09 13:19:41 by juvan-to      #+#    #+#                 */
-/*   Updated: 2024/04/09 14:31:52 by juvan-to      ########   odam.nl         */
+/*   Updated: 2024/04/11 16:41:51 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,16 @@ Server::Server(void)
 	return;
 }
 
-void	Server::config(void)
+Server::~Server(void)
 {
-	int	status;
+	std::cout << "Closing server socket." << std::endl;
+	close(this->_listenFd);
+	return;
+}
 
-	this->_serverAddress.sin_family = AF_INET;
-	this->_serverAddress.sin_addr.s_addr = INADDR_ANY;
-	this->_serverAddress.sin_port = htons(PORT);
-
-	// Step 1: create a socket
+// Step 1: create a socket
+void	Server::createSocket(void)
+{
 	// sa.sin_family or AF_INET
 	this->_listenFd = socket(this->_serverAddress.sin_family , SOCK_STREAM, 0);
 	if (this->_listenFd == -1) {
@@ -40,16 +41,19 @@ void	Server::config(void)
 		return;
 	}
 	std::cout << "Created server socket fd = " << this->_listenFd << std::endl;
-	//
+}
 
-	// Step 2: Identify a socket
+// Step 2: Identify a socket
+void	Server::bindSocket(void)
+{
+	int	status;
+	
 	status = bind(this->_listenFd, reinterpret_cast<struct  sockaddr*>(&this->_serverAddress), sizeof(this->_serverAddress));
 	if (status != 0) {
 		std::cerr << "bind error: " << std::strerror(errno) << std::endl;
 		return ;
 	}
 	std::cout << "Bound socket to localhost port " << PORT << std::endl;
-	//
 
 	// Step 3: Wait for incoming connection
 	std::cout << "Listening on port " << PORT << std::endl;
@@ -60,52 +64,64 @@ void	Server::config(void)
 	}
 }
 
-Server::~Server(void)
+void	Server::config(void)
 {
-	std::cout << "Closing server socket." << std::endl;
-	close(this->_listenFd);
-	return;
+
+	this->_serverAddress.sin_family = AF_INET;
+	this->_serverAddress.sin_addr.s_addr = INADDR_ANY;
+	this->_serverAddress.sin_port = htons(PORT);
+
+	this->createSocket();
+	this->bindSocket();
+}
+
+void	Server::acceptConnection(void)
+{
+	struct sockaddr_storage	client_addr;
+	socklen_t				addr_size;
+	int						client_fd;
+	addr_size = sizeof(client_addr);
+	client_fd = accept(this->_listenFd, reinterpret_cast<struct sockaddr *>(&client_addr), &addr_size);
+	if (client_fd == -1) {
+		std::cerr << "client fd error: " << std::strerror(errno) << std::endl;
+		return ; 
+	}
+	std::cout << "------------------------------------------------" << std::endl;
+	std::cout << "Accepted new connection on client socket fd: " << client_fd << std::endl;
+}
+
+void	Server::processConnection(int client_fd)
+{
+	char	buffer[BUFSIZ];
+	int		bytes_read;
+	
+	bytes_read = 1;
+	while (bytes_read >= 0)
+	{
+		std::cout << "Reading client socket " << client_fd << std::endl;
+		bytes_read  = recv(client_fd, buffer, BUFSIZ, 0);
+		if (bytes_read == 0) {
+			std::cout << "Client socket " << client_fd << " closed the connection." << std::endl;
+			break;
+		}
+		else if (bytes_read == -1) {
+			std::cerr << "recv error " << std::strerror(errno) << std::endl;
+			break;
+		}
+		else
+			this->handleRequest(buffer);
+
+	}
+	std::cout << "Closing client socket." << std::endl;
+	close(client_fd);
+	std::cout << "------------------------------------------------" << std::endl;
 }
 
 void	Server::run(void)
 {
-	struct sockaddr_storage	client_addr;
-	socklen_t				addr_size;
-	char					buffer[BUFSIZ];
-	int						bytes_read;
-	int						client_fd;
-
 	while (1)
 	{
-		addr_size = sizeof(client_addr);
-		client_fd = accept(this->_listenFd, reinterpret_cast<struct sockaddr *>(&client_addr), &addr_size);
-		if (client_fd == -1) {
-			std::cerr << "client fd error: " << std::strerror(errno) << std::endl;
-			return ; 
-		}
-		std::cout << "------------------------------------------------" << std::endl;
-		std::cout << "Accepted new connection on client socket fd: " << client_fd << std::endl;
-
-		bytes_read = 1;
-		while (bytes_read >= 0)
-		{
-			std::cout << "Reading client socket " << client_fd << std::endl;
-			bytes_read  = recv(client_fd, buffer, BUFSIZ, 0);
-			if (bytes_read == 0) {
-				std::cout << "Client socket " << client_fd << " closed the connection." << std::endl;
-				break;
-			}
-			else if (bytes_read == -1) {
-				std::cerr << "recv error " << std::strerror(errno) << std::endl;
-				break;
-			}
-			else
-				this->handleRequest(buffer);
-
-		}
-		std::cout << "Closing client socket." << std::endl;
-		close(client_fd);
-		std::cout << "------------------------------------------------" << std::endl;
+		this->acceptConnection();
 	}
 	return;
 }
