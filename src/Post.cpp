da437949825d8bd71017493ed64bf30ad95fa752
juliavantol <juliavantol@live.nl>
2024-04-25 00:17:18 +0200

    save

--- src/Post.cpp
+++ src/Post.cpp
@@ -6,7 +6,7 @@
 /*   By: Julia <Julia@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/23 23:05:36 by Julia         #+#    #+#                 */
-/*   Updated: 2024/04/24 16:57:13 by juvan-to      ########   odam.nl         */
+/*   Updated: 2024/04/25 00:17:03 by Julia         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,21 @@ void	Server::postRequest(std::string path, char *buffer)
 	pid_t	pid;
 
 	pipe(fds);
+	// std::cout << buffer << std::endl;
 	pid = fork();
-
 	if (pid == 0)
 	{
-		dup2(fds[0], STDIN_FILENO);
-		close(fds[0]);
 		close(fds[1]);
 
-		char *const envp[] = {nullptr};
+		const char *contentType = "CONTENT_TYPE: multipart/form-data; boundary=----WebKitFormBoundaryBIeRFsXZV9QqAn3y";
+		const char *contentLength = "CONTENT_LENGTH: 201";
+		char *const envp[] = {const_cast<char*>(contentType), const_cast<char*>(contentLength), nullptr};
+		
+		dup2(fds[0], STDIN_FILENO);
+		close(fds[0]);
+		
 		const char *args[] = {"./cgi-bin/upload.py", nullptr};
+		
     	execve("./cgi-bin/upload.py", const_cast<char**>(args), envp);
 		exit(EXIT_FAILURE);
 	}
