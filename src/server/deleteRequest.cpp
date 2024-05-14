645a0f9c37d83219eda12a47e1e0846c8b7f1926
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2024-05-14 16:07:58 +0200

    delete requests work

--- src/server/deleteRequest.cpp
+++ src/server/deleteRequest.cpp
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/14 15:11:58 by juvan-to      #+#    #+#                 */
-/*   Updated: 2024/05/14 15:16:05 by juvan-to      ########   odam.nl         */
+/*   Updated: 2024/05/14 16:07:25 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,20 @@
 
 void	Server::deleteRequest(std::string file)
 {
-	std::cout << file << std::endl;
+	std::string	fullFilePath = "";
+	int			result;
+
+	fullFilePath = "cgi-bin/uploads" + file;
+	std::cout << fullFilePath << std::endl;
+	result = std::remove(fullFilePath.c_str());
+	if (result == 0)
+	{
+		std::cout << "File succesfully deleted" << std::endl;
+	}
+	else
+	{
+		perror("Error deleting file");
+	}
 	std::string response = serveFile("html/files.html", "200 OK", GREEN);
     write(this->_clientFd, response.c_str(), response.size());
     terminalMessage("Server response ", response);
