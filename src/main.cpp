21cdb2016777f9f1480a93ef10a6d74eeccb1535
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2024-05-22 13:48:47 +0200

    save

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: psadeghi <psadeghi@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/09 13:49:54 by juvan-to          #+#    #+#             */
-/*   Updated: 2024/05/21 14:23:54 by psadeghi         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: psadeghi <psadeghi@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/04/09 13:49:54 by juvan-to      #+#    #+#                 */
+/*   Updated: 2024/05/22 13:47:51 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ int main(int argc, char** argv)
 			serverManager.configFile(DEFAULT_PATH);
 		while(1)
 		{
-			std::cout << "First time \n" << std::endl;
 			serverManager.config();
 			serverManager.preparePoll();
 			serverManager.setUpPoll();
