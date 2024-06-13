3e0857a6c76aa5387ecb224fed3c30ef7315f632
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2024-06-13 16:40:57 +0200

    save

--- cgi-bin/directoryListing.py
+++ cgi-bin/directoryListing.py
@@ -7,3 +7,8 @@ directory_path = os.getenv("INPUT")
 print("<html><body><h1>Index of ")
 print(directory_path)
 print("</h1>")
+
+cwd = os.getcwd()
+dir = os.path.join(cwd, directory_path)
+print(dir)
+print("</body></html>")
