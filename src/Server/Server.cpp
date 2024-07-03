5885f58b904472a927234a7fc5d030cff8179e65
Mirjam Goedkoop <mgoedkoo@f0r3s9.codam.nl>
2024-07-03 13:09:56 +0200

    deleted error pages

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: mgoedkoo <mgoedkoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:13:40 by mgoedkoo          #+#    #+#             */
-/*   Updated: 2024/06/21 15:56:28 by mgoedkoo         ###   ########.fr       */
+/*   Updated: 2024/07/03 12:59:17 by mgoedkoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,53 +60,26 @@ void	Server::initDefaultLocation(void)
 
 void	Server::initErrorPages(void)
 {
-	errorPages[400] = "html/error_pages/BadRequest.html"; // handled
-	// errorPages[401] = "html/error_pages/unauthorized.html";
-	errorPages[403] = "html/error_pages/Forbidden.html"; // handled
-	errorPages[404] = "html/error_pages/PageNotFound.html"; // handled
-	errorPages[405] = "html/error_pages/MethodNotAllowed.html"; // handled
-	// errorPages[406] = "html/NotAcceptable.html";
-	// errorPages[407] = "html/ProxyAuthenticationRequired.html";
-	errorPages[408] = "html/error_pages/RequestTimeout.html"; // handled
-	// errorPages[409] = "html/Conflict.html";
-	errorPages[411] = "html/error_pages/LengthRequired.html"; // handled
-	errorPages[413] = "html/error_pages/PayloadTooLarge.html"; // handled
-	// errorPages[414] = "html/URITooLong.html";
-	errorPages[415] = "html/error_pages/UnsupportedMediaType.html"; // handled
-	// errorPages[431] = "html/error_pages/RequestHeaderFieldsTooLarge.html";
+	errorPages[400] = "html/error_pages/BadRequest.html";
+	errorPages[403] = "html/error_pages/Forbidden.html";
+	errorPages[404] = "html/error_pages/PageNotFound.html";
+	errorPages[405] = "html/error_pages/MethodNotAllowed.html";
+	errorPages[411] = "html/error_pages/LengthRequired.html";
+	errorPages[413] = "html/error_pages/PayloadTooLarge.html";
+	errorPages[415] = "html/error_pages/UnsupportedMediaType.html";
 	errorPages[500] = "html/error_pages/InternalServerError.html";
-	// errorPages[501] = "html/error_pages/NotImplemented.html";
-	// errorPages[502] = "html/error_pages/BadGateway.html";
-	// errorPages[503] = "html/error_pages/ServiceUnavailable.html";
-	// errorPages[504] = "html/error_pages/GatewayTimeout.html";
-	errorPages[505] = "html/error_pages/HTTPVersionNotSupported.html"; // handled
-	// errorPages[507] = "html/error_pages/InsufficientStorage.html";
-	// errorPages[508] = "html/error_pages/LoopDetected.html";
-	// errorPages[511] = "html/error_pages/NetworkAuthenticationRequired.html";
+	errorPages[505] = "html/error_pages/HTTPVersionNotSupported.html";
 
 	errorMessages[400] = "400 Bad Request";
-	errorMessages[401] = "401 Unauthorized";
 	errorMessages[403] = "403 Forbidden";
 	errorMessages[404] = "404 Not Found";
 	errorMessages[405] = "405 Method Not Allowed";
-	errorMessages[406] = "406 Not Acceptable";
-	errorMessages[407] = "407 Proxy Authentication Required";
 	errorMessages[408] = "408 Request Timeout";
-	errorMessages[409] = "409 Conflict";
 	errorMessages[411] = "411 Length Required";
 	errorMessages[413] = "413 Payload Too Large";
-	errorMessages[414] = "414 URI Too Long";
 	errorMessages[415] = "415 Unsupported Media Type";
-	errorMessages[431] = "431 Request Header Fields Too Large";
 	errorMessages[500] = "500 Internal Server Error";
-	errorMessages[501] = "501 Not Implemented";
-	errorMessages[502] = "502 Bad Gateway";
-	errorMessages[503] = "503 Service Unavailable";
-	errorMessages[504] = "504 Gateway Timeout";
 	errorMessages[505] = "505 HTTP Version Not Supported";
-	errorMessages[507] = "507 Insufficient Storage";
-	errorMessages[508] = "508 Loop Detected";
-	errorMessages[511] = "511 Network Authentication Required";
 }
 
 void	Server::checkServer(void)
