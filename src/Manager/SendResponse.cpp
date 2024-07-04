c5891240296fe6a849447a58b6e123c7f02be802
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2024-07-04 14:26:43 +0200

    write error checks

--- src/Manager/SendResponse.cpp
+++ src/Manager/SendResponse.cpp
@@ -6,7 +6,7 @@
 /*   By: mgoedkoo <mgoedkoo@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/21 13:21:13 by juvan-to      #+#    #+#                 */
-/*   Updated: 2024/07/03 15:38:31 by juvan-to      ########   odam.nl         */
+/*   Updated: 2024/07/04 14:26:04 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,33 @@
 
 void	Manager::sendResponse(std::string response, int clientFd)
 {
-	write(clientFd, response.c_str(), response.size());
+	ssize_t			bytesWritten;
+
+	bytesWritten = write(clientFd, response.c_str(), response.size());
+	if (bytesWritten < 0)
+	{
+		closeClientConnection(clientFd);
+		return;
+	}
+	else if (bytesWritten == 0)
+		return;
 	serverMessage(response, clientFd, GREEN);
 }
 
 bool	Manager::sendErrorResponse(std::string response, int clientFd)
 {
-	std::string errorLine;
+	std::string		errorLine;
+	ssize_t			bytesWritten;
 
-	write(clientFd, response.c_str(), response.size());
+
+	bytesWritten = write(clientFd, response.c_str(), response.size());
+	if (bytesWritten < 0)
+	{
+		closeClientConnection(clientFd);
+		return (false);
+	}
+	else if (bytesWritten == 0)
+		return (true);
 	serverMessage(response, clientFd, RED);
 	errorLine = response.substr(0, response.find("\r\n"));
 	if (errorLine == "HTTP/1.1 413 Payload Too Large")
